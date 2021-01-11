b6cb3a5850566dfaaf27e668ee8cd41cebb944c2
Vlad Dr.Frame <remenjuk2010@gmail.com>
2021-01-11 08:38:26 +0200

    done

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import a from "./js/a";
+import "./css/section.scss";
+import "./css/body.scss";
+import "./css/style.scss";
+
+const formul = (a, b) => {
+  console.log(a + b);
+};
+
+formul(5, 10);
---
+++ src/js/a.js
@@ -0,0 +1,2 @@
+console.log("gfgfjdkgdfjkbdfngd");
+const a = "gfdg";
